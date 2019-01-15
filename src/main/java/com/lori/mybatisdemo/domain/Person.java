8a9d56882e5aa1fc2501302832440dcad9d9c29e
lori <81104174@qq.com>
2019-01-16 07:53:18 +0800

    初始化

---
+++ src/main/java/com/lori/mybatisdemo/domain/Person.java
@@ -0,0 +1,95 @@
+package com.lori.mybatisdemo.domain;
+
+import java.util.Date;
+
+public class Person {
+    private Integer id;
+
+    private String name;
+
+    private Integer age;
+
+    private Integer sex;
+
+    private Date createTime;
+
+    private String createUser;
+
+    private Date updateTime;
+
+    private String updateUser;
+
+    private Integer yn;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Integer getSex() {
+        return sex;
+    }
+
+    public void setSex(Integer sex) {
+        this.sex = sex;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getCreateUser() {
+        return createUser;
+    }
+
+    public void setCreateUser(String createUser) {
+        this.createUser = createUser == null ? null : createUser.trim();
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public String getUpdateUser() {
+        return updateUser;
+    }
+
+    public void setUpdateUser(String updateUser) {
+        this.updateUser = updateUser == null ? null : updateUser.trim();
+    }
+
+    public Integer getYn() {
+        return yn;
+    }
+
+    public void setYn(Integer yn) {
+        this.yn = yn;
+    }
+}
---
+++ src/main/resources/sql/table.sql
@@ -0,0 +1,13 @@
+create schema mybatis_demo default character set utf8 collate utf8_general_ci;
+
+create table person(
+  id int primary key auto_increment comment 'ID',
+  name varchar(30) not null comment '名字',
+  age int(3) not null comment '年龄',
+  sex int(1) default 1 comment '性别: 1 男  0 女',
+  create_time timestamp not null default current_timestamp comment '创建时间',
+  create_user varchar(100) null comment '创建人',
+  update_time timestamp not null default current_timestamp on update current_timestamp comment '更新时间',
+  update_user varchar(100) null comment '更新人',
+  yn int(1) default 1 comment '是否有效: 1 有效  0 无效'
+);
